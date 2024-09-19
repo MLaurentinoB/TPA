0436bca9daff9738ce47b7ffbb297ff8a4046452
Miguel Laurentino <104170938+MLaurentinoB@users.noreply.github.com>
2024-09-19 20:49:18 -0300

    Delete tpa1406 directory

--- tpa1406/src/tpa1406/ex2.java
+++
@@ -1,7 +0,0 @@
-package tpa1406;
-import java.util.Scanner;
-public class ex2 {
-public static void main(String[] args) {
-	
-}
-}
--- tpa1406/tpa 2405/src/Triângulo.java
+++
@@ -1,6 +0,0 @@
-import java.util.Scanner;
-public class Triângulo {
-public static void main(String[] args) {
-	Scanner ler = new Scanner (System.in);
-}
-}
